a89ac593004d5313dcd14f38632348ef5b4a7c3c
ASHRAFK2020 <ashrafmostafakamal@yahoo.com>
2024-01-30 17:27:07 +0200

    empty

--- Program.cs
+++ Program.cs
@@ -7,20 +7,10 @@ namespace _11new
     {
         static void Main(string[] args)
         {
-            var namesList = new List<string>();
-           dynamic myDynamicVariable = 5;
-           System.Console.WriteLine(myDynamicVariable);
-           myDynamicVariable = "ashraf";
-           System.Console.WriteLine(myDynamicVariable);
-           myDynamicVariable = new List<float>();
-           myDynamicVariable.Add(10f);
-           myDynamicVariable.Add(5.2f);
-           myDynamicVariable.Add(3.4f);
-    
-           foreach (var item in myDynamicVariable)
-           {
-                System.Console.WriteLine(item);
-           }
+          
+            PrintNewMassage("hello my name is ashraf");
+           
+          
         }
 
 
