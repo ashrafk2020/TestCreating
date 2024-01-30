86aa9c4b07ec8410fab17d4c3e7ec1a46bd8c599
ASHRAFK2020 <ashrafmostafakamal@yahoo.com>
2024-01-30 16:17:56 +0200

    Initial start

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+
+namespace _11new
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           PrintNewMassage("the new message here");
+           PrintNewMassage("i am ashraf");
+           PrintNewMassage("hello saif!");
+
+        }
+
+
+        public static void  PrintNewMassage(string message)
+        {
+            System.Console.WriteLine(message);
+        }
+    }
+
+
+    
+}
